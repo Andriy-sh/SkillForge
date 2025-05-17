a490fd04f193ae8826b79da6c3a1352098331c58
Andriy-sh <152734833+Andriy-sh@users.noreply.github.com>
2025-05-17 15:55:44 +0300

    feat: enhance ESLint configuration with rules for generated files and specific overrides

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,36 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: [
+      "**/generated/**", // Ignore all generated files
+    ],
+  },
+  {
+    // Apply these rules to all files
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    rules: {
+      // Your global rules here
+    },
+  },
+  {
+    // Disable rules for generated Prisma files
+    files: ["**/generated/**/*.{js,ts}"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-unsafe-function-type": "off",
+      "@typescript-eslint/no-wrapper-object-types": "off",
+      "@typescript-eslint/no-unnecessary-type-constraint": "off",
+    },
+  },
+  {
+    // Specific override for wasm.js
+    files: ["**/wasm.js"],
+    rules: {
+      "@typescript-eslint/no-require-imports": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,7 @@ const nextConfig: NextConfig = {
       "avatars.githubusercontent.com",
     ],
   },
+  
 };
 
 export default nextConfig;
